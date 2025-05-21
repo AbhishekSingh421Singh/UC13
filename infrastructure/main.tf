bbbca2e4c6ac21c2e83e258e051fe0d684922d4d
AbhishekSingh421Singh <73700379+AbhishekSingh421Singh@users.noreply.github.com>
2025-05-21 10:19:49 +0000

    Added src as regular folder with lambda code

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,6 +1,6 @@
 # IAM Role for Lambda
 resource "aws_iam_role" "lambda_exec_role" {
-  name = "lambda_exec_role"
+  name = "lambda_exec_role12"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
